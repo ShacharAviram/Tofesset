73fdb2ebe1770d84cc88518424b76ba4309daeed
Shachar Aviram <83129363+shacharaviram@users.noreply.github.com>
2021-12-09 14:55:44 +0200

    initial API for IP module and IE module

---
+++ IndicationAndExperience/Indicator.py
@@ -0,0 +1,19 @@
+"""
+    This file is the manager of the camera that we use to catch players
+    The camera needs to produce images in a constant pace
+"""
+
+
+class ImageProcessor:
+    def __init__(self):
+        """
+        initialize an instance of an indicator
+        """
+        pass
+
+    def indicate(self) -> None:
+        """
+        indicates the player that he has been caught
+        :return: None
+        """
+        pass
