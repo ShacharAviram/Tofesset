5ba444a5c5e20a3b508fbcbdbaed25984139f968
ItayRiv <itayrivkin@gmail.com>
2021-12-22 20:25:29 +0200

    Totally new server build finally works!!!

--- Communication/Server.py
+++ Communication/Server.py
@@ -1,86 +1,125 @@
 import socket
-import time
-"""
-    This file contains the code that operates the server.
-    it should contain messages handling code, data structure for the game and error handling functions.
+import select
 
-    https://www.youtube.com/watch?v=MbXWrmQW-OE
-"""
+HEADER_LENGTH = 10
 
-TOFFESS = 1
-FREE = 0
-CAUGHT = 2
+IP = "127.0.0.1"
+PORT = 5050
 
+# Create a socket
+# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-class Server:
-    def __init__(self):
-        self.HOST = ''
-        self.PORT = 5050
-        self.database = dict()
-        self.num_of_players = 0
+# Bind, so server informs operating system that it's going to use given IP and port
+# For a server using 0.0.0.0 means to listen on all available interfaces, useful to connect locally to 127.0.0.1 and remotely to LAN interface IP
+server_socket.bind((IP, PORT))
 
-    def initialize_server(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self.HOST, self.PORT))
-            s.listen(1)
-            ind = 0
-            while ind <= 2:
-                conn, addr = s.accept()
-                with conn:
-                    if addr[0] not in self.database:
-                        self.database[addr[0]] = 0
-                        print(self.database)
-                        print('Connected by', addr[0])
-                        data = conn.recv(4096)
-                        # print(data)
-                        conn.sendto(b'0', addr)
+# This makes server listen to new connections
+server_socket.listen()
 
-                if data:
-                    self.change_database(addr[0], data)
-                    print(self.database)
+# List of sockets for select.select()
+sockets_list = [server_socket]
 
-    def initial_connection(ind):
-        conn, addr = s.accept()
-        with conn:
-            database[addr[0]] = [0, conn]
-            print(database)
-            print('Connected by', addr[0])
-            data = conn.recv(4096)
-            print(data)
-            ind += 1
-            conn.sendto(b'0', addr)
-            return ind, conn, addr
+# List of connected clients - socket as a key, user header and name as data
+clients = {}
+
+print(f'Listening for connections on {IP}:{PORT}...')
+
+database = dict()
 
 
-    def check_for_message():
-        """if .recv(4096):
-            return True
+# Handles message receiving
+def receive_message(client_socket):
+    try:
+
+        # Receive our "header" containing message length, it's size is defined and constant
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+        if not len(message_header):
+            return False
+
+        # Convert header to int value
+        message_length = int(message_header.decode('utf-8').strip())
+
+        # Return an object of message header and message data
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+        # If we are here, client closed connection violently, for example by pressing ctrl+c on his script
+        # or just lost his connection
+        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
+        # and that's also a cause when we receive an empty message
+        return False
+
+
+while True:
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+    for notified_socket in read_sockets:
+        # If notified socket is a server socket - new connection, accept it
+        if notified_socket == server_socket:
+
+            # Accept new connection
+            # That gives us new socket - client socket, connected to this given client only, it's unique for that client
+            # The other returned object is ip/port set
+            client_socket, client_address = server_socket.accept()
+            # Client should send his name right away, receive it
+            user = receive_message(client_socket)
+            database[user['data'].decode('utf-8')] = 0
+            # If False - client disconnected before he sent his name
+            if user is False:
+                continue
+
+            # Add accepted socket to select.select() list
+            sockets_list.append(client_socket)
+
+            # Also save username and username header
+            clients[client_socket] = user
+
+            print('Accepted new connection from {}:{}, username: {}'.format(*client_address,
+                                                                            user['data'].decode('utf-8')))
+        # Else existing socket is sending a message
         else:
-            return False"""
 
+            # Receive message
+            message = receive_message(notified_socket)
+            # print(message) unnecessary
+            # If False, client disconnected, cleanup
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
 
-    def change_database(self, addr, status_change):
-        self.database[addr] = status_change
-        return self.database
+                # Remove from list for socket.socket()
+                sockets_list.remove(notified_socket)
 
+                # Remove from our list of users
+                del clients[notified_socket]
 
+                continue
 
+            # Get user by notified socket, so we will know who sent the message
+            user = clients[notified_socket]
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+            # Update database
+            if message["data"].decode("utf-8") in '012':
+                database[user["data"].decode("utf-8")] = message["data"].decode("utf-8")
+            # Iterate over connected clients and broadcast message
+            for client_socket in clients:
+                # Send it only to reffered to client
+                if clients[client_socket] == message:
+                    # Send user and message (both with their headers)
+                    # We are reusing here message header sent by sender, and saved username header send by user when he connected
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+                    # Update database with info catcher
+                    database[message['data'].decode('utf-8')] = 2
+        print(database)
+    # It's not really necessary to have this, but will handle some socket exceptions just in case
+    for notified_socket in exception_sockets:
+        # Remove from list for socket.socket()
+        sockets_list.remove(notified_socket)
 
-    def create_catcher(ind):
-        if ind == 1:
-            database[0] = 1
-
-
-    def send_message():
-        ...
-
-
-
-
-
-if __name__ == '__main__':
-    server = Server()
-    Server.initialize_server(server)
-
+        # Remove from our list of users
+        del clients[notified_socket]
 
 
