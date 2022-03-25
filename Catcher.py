647c5055bec0897ce4bf0d92dbbe94aa3ee3ccbd
yaronbsw <yaronbsw@gmail.com>
2022-03-25 15:42:37 +0300

    fulfilled all of Geva's notes except for checking functionality with actual client.

--- Catcher.py
+++ Catcher.py
@@ -1,9 +1,10 @@
 import socket
-import select
-from ImageProcess.ImageProcessor import ImageProcessor
-from Constants import *
 import bluetooth
 import time
+from ImageProcess.ImageProcessor import ImageProcessor
+from Constants import *
+from IndicationAndExperience.Indicator_catcher_API import Indicator
+
 
 
 hostMACAddress = '8c:c8:4b:80:50:42'  # The MAC address of a Bluetooth adapter on the server.
@@ -16,23 +17,20 @@ s = bluetooth.BluetoothSocket()
 s.bind((hostMACAddress, port))
 s.listen(backlog)
 
-# print(f'Listening for connections on {IP}:{PORT}...')
 
-# Current player status
+# Current player status data structure
 database = dict()
 # connection list of players (client1, client2, client3, client4)
 player_list = [False, False, False, False]
 
 
-def get_player_address_list(dictionary):
+def get_player_address(dictionary, player_name):
     """
+    :param player_name: the name of the player
     :param dictionary: database of players in game
-    :return: List of mac addresses of all players
+    :return: mac addresses of given player
     """
-    mylist = []
-    for key in dictionary.keys():
-        mylist.append(dictionary[key][0])
-    return mylist
+    return dictionary[player_name][0]
 
 
 def set_database(player_dictionary, player_name, status):
@@ -50,24 +48,29 @@ def is_caught():
     check if any player is caught, if so:
     1. change player status in database
     2. send message to client
-    :return:
+    3. wait for eco from client, else, (after 3 seconds) turn to IndicationAndExperience
+    :return: None
     """
     # check if a player is considered caught, send message and wait for validation
-    # assuming the received data is in the following format: (bool, id)
     for returned_data in ImageProcessor.ImageProcessor.return_data():
         player_is_caught = returned_data[0]
         if player_is_caught:
             set_database(database, returned_data[1], 1)  # set player status as caught
             # send message to client and find relevant id address
-            for client in get_player_address_list(database):
-                if client == returned_data[1]:
-                    # send relevant message to client
-                    client.send(int_to_bytes(PLAYER_CAUGHT))
-
-                while not(client.recv(1024)):
-                    print('waiting for validation')
-                    client.send(int_to_bytes(PLAYER_CAUGHT))
-                    print('sent another message')
+            caught_player_mac_address = get_player_address(database, returned_data[1])
+            caught_player_mac_address.send(int_to_bytes(PLAYER_CAUGHT))
+            # try to send message for at most 3 whole seconds
+            iteration = 0
+            while not(client.recv(1024)) and iteration <= 9:
+                iteration += 1
+                print('waiting for validation')
+                client.send(int_to_bytes(PLAYER_CAUGHT))
+                print('sent another message')
+                time.sleep(0.3)
+            # do we want to do something if message wasn't received?
+            if iteration == 10:
+                # do something in IndicationAndExperience...?
+                pass
         else:
             pass
 
@@ -76,36 +79,41 @@ def configure_connections():
     """
     1. set up in order for the game to function properly
     2. connection set up
-    -calls reset game in end
-    :return:
+    3. calls reset_game() in end
+    :return: None
     """
-    # connect players - maximum five with break option
-    for i in range(1, 5):
-        client_mac, client_port = s.accept()
-        if client:
-            name = client_mac.recv(1024)
-            database[name] = [client_mac, 0]
-            player_list[i] = True
+    # connect players - maximum four with break option
+    while len(database) <= 4 and not(Indicator.Indicator.is_reset_button_pressed()):
+        try:
+            client_mac, client_port = s.accept()
+            name = client_mac.recv(1024)         # receive name from player
+            database[name] = [client_mac, 0]     # mac address and starting status
+            player_list[len(database)-1] = True  # "len(database)-1" index in list
             check_game_button(database)
-        else:
+        except bluetooth.BluetoothError:
             break
     # send each player a 'start' message
-    for player in database:
-        database.get(player)[0].send(START)
-
+    else:
+        for player in database:
+            player_address = get_player_address(database, player)
+            player_address.send(START)
+    # reset player's status
+    reset_game(database)
 
 def check_game_button(player_dict):
     """
-    check if game_button was pressed
-    :return: True
+    :param player_dict: dictionary with player info
+    check if reset_game_button was pressed, if so, reset all player's status
+    :return: True or None
     """
-    if game_button() is True:
+    if Indicator.Indicator.is_reset_button_pressed() is True:
         reset_game(player_dict)
         return True
 
 
 def reset_game(player_dict):
     """
+    :param player_dict: dictionary with player info
     be called (1) after configure_connections or (2) after the game if reset button pressed.
     1. initialize lights in catcher
     2. resets the database status for players
@@ -145,48 +153,44 @@ def check_game_end():
 
 def game_loop():
     """
+    while the reset button isn't presses and the game hasn't ended yet -
     1. calculates "is caught"
-    2. sends message and verify receiving
-    3. updates my own database
-    *checks for game_end() and end loop if so, wait for game_button() to be pressed -> start game again.
-    :return:
+        1. checks if player was caught
+        2. sends message and verify receiving
+        3. updates the database
+    2. wait 0.3 seconds
+    :return: None
     """
-    while game_button() and check_game_end():
+    while not(Indicator.Indicator.is_reset_button_pressed()) and check_game_end():
         # check if player is caught
         is_caught()
         # wait 0.3 sec before rerunning loop
         time.sleep(0.3)
 
 
-# game loop
-try:
-    # set up in order for the game to function properly connection set up
-    configure_connections()
-    # checks if player is caught (via is_caught()) and reacts accordingly while game is played
-    game_loop()
-
-except ConnectionError:
-    # print("Closing socket")
-    s.close()
-
-
 if __name__ == "__main__":
-    configure_connections()
-    game_loop()
+    try:
+        # set up in order for the game to function properly connection set up
+        configure_connections()
+        # checks if player is caught (via is_caught()) and reacts accordingly while game is played
+        game_loop()
+
+    except ConnectionError:
+        # print("Closing socket")
+        s.close()
 
 
-    # Todo: 21/3/22
-    # 1. insert all starting progress to configure_connections() function
-    # 2. make option to enter with less than 4 systems
-    # 3. create open functions (description in the code)
-    # 4. create game_loop.
+# Todo: 21/3/22
+# 1. insert all starting progress to configure_connections() function
+# 2. make option to enter with less than 4 systems
+# 3. create open functions (description in the code)
+# 4. create game_loop.
 
-    # TODO: until 28/3/22
-    # 1. check with actual client
-    # 2. make real game loop.
-    # configure connection - נעבור על זה ביחד
-    # 3. pass all Constants to Constants file
-    # 4. line 43 - change to const.....................***נראה לי שהושלם***
-    # limit line 51 loop (מפחד שניתקע שם לנצח)
-
-
+# TODO: until 28/3/22
+# 1. check with actual client
+# 2. make real game loop.......................Done (most of the loop is executed ny is_caught())
+# configure connection.........................Done (conformation needed)
+# 3. pass all Constants to Constants file......Done (START, PLAYER_CAUGHT)
+# 4. line 43 - change to const.................Done (conformation needed)
+# limit line 51 loop...........................Done (10 tries, altogether 3 whole seconds)
+# line 52 - question towards Geva
