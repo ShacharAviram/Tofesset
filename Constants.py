50551a8f3b132453a391605e4ff7fae217fafc64
gevabelz <geva.belz.42@gmail.com>
2022-04-07 16:21:17 +0300

    Change server comm protocol to SELECT-THREADS

--- Constants.py
+++ Constants.py
@@ -1,7 +1,4 @@
-# Game Rules
-CATCH_DISTANCE = 1
-
-# GPIO PINS
+#GPIO PINS
 RED_PIN = 5
 GREEN_PIN = 7
 BLUE_PIN = 6
@@ -9,32 +6,27 @@ RESET_BUTTON_PIN = 8
 CATCHER_PINS = [1, 2, 3, 4]
 AUDIO_PIN = 10
 
-# Audio files dir
+#Audio files dir
 CAUGHT_SOUND = 'sounds/caught.wav'
 CONNECTED_SOUND = 'sounds/connected.wav'
-WAKE_UP_BEEP = '---add here---'
 
-# General Constants
+#General Constants
 PLAYER_FREE = 10
 START = 11
 PLAYER_CAUGHT = 12  # for player and server use
 
-# Communication
-SERVER_MAC = '4C:77:CB:6F:32:F8'
+#Communication
+SERVER_MAC = '8c:c8:4b:80:50:42'
 COMMUNICATION_PORT = 5
-BUFFER_SIZE = 128
 
-# Systems IDs
-ID_1 = 141
-ID_2 = 592
-ID_3 = 653
-ID_4 = 589
+#Systems IDs
+ID_1 = 7
+ID_2 = 8
+ID_3 = 9
+ID_4 = 10
 
-# system ID list
-ID_LIST = [ID_1, ID_2, ID_3, ID_4]
-
-# Game Modes
+#Game Modes
 CATCH_EM_ALL = 333
 RISE_OF_THE_DEAD = 444
 GAME_MODES = [CATCH_EM_ALL, RISE_OF_THE_DEAD]
-KNOCK_OUT_TIME = 30  # (IN SECONDS)
+KNOCK_OUT_TIME = 30 #(IN SECONDS)
--- Player.py
+++ Player.py
@@ -1,16 +1,20 @@
+import time
 import bluetooth
-from Indicator import Indicator
-import Constants
+from IndicationAndExperience.Indicator_player_API import Indicator
+from Constants import *
 
 
 class Player:
     def __init__(self):
         """ Creates a player """
-        self.serverMACAddress = '8c:c8:4b:80:50:42'  # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
-        self.port = 5
+        self.serverMACAddress = SERVER_MAC  # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+        self.port = COMMUNICATION_PORT
         self.s = bluetooth.BluetoothSocket()
-        self.PLAYER_STATUS = 0  # Free
-        self.ID = num  # configure to each system
+        self.PLAYER_STATUS = None  # Free
+        self.ID = ID_1  # configure to each system
+        self.Indicator = Indicator()
+        self.connected_flag = False
+        self.game_mode = None
 
     def int_to_bytes(self, number: int) -> bytes:
         return number.to_bytes(length=(8 + (number + (number < 0)).bit_length()) // 8, byteorder='big', signed=True)
@@ -19,38 +23,59 @@ class Player:
         return int.from_bytes(xbytes, 'big')
 
     def make_connection(self):
-        if self.s.connect_ex((self.serverMACAddress, self.port)) != 0:
-            for i in range(5):
-                if self.s.connect_ex((self.serverMACAddress, self.port)) == 0:
-                    return self.s.connect_ex((self.serverMACAddress, self.port))
-        return self.s.connect_ex((self.serverMACAddress, self.port))
+        for i in range(5):
+            if self.s.connect_ex((self.serverMACAddress, self.port)) == 0:
+                self.s.send(self.int_to_bytes(self.ID))
+                data = self.int_from_bytes(self.s.recv(1024)) #Confirmation message from server = game_mode
+                if data in GAME_MODES:
+                    self.game_mode = data
+                else:
+                    raise ConnectionError
+                self.Indicator.turn_on_LED(BLUE_PIN)
+                self.Indicator.play_sound(CONNECTED_SOUND)
+                self.connected_flag = True
+                return True
+            time.sleep(0.5)
+        raise ConnectionError
 
     def check_msg(self, data):
         msg = self.int_from_bytes(data)
         return msg
 
     def play(self):
-        if self.s.connect_ex((self.serverMACAddress, self.port)) == 0:  # Change to make connection function.
-            Indicator.turn_on_led(Constants.BLUE_PIN)
-            Indicator.play_sound(Constants.CONNECTED_SOUND)
-            self.s.send(self.int_to_bytes(self.ID))
-            while True:
-                data = self.s.recv(1024)  #TODO: check if player can get more than one msg
-                if self.check_msg(data) == Constants.START:
-                    self.PLAYER_STATUS = 0
-                    Indicator.turn_on_led(Constants.GREEN_PIN)
-                    self.s.send(self.int_to_bytes(Constants.START))
-                elif self.check_msg(data) == Constants.PLAYER_CAUGHT:
-                    self.PLAYER_STATUS = 1
-                    Indicator.turn_on_led(Constants.RED_PIN)
-                    Indicator.play_sound(Constants.CAUGHT_SOUND)
-                    return_text = self.int_to_bytes(Constants.PLAYER_CAUGHT)
-                    self.s.send(return_text)
-            #### self.s.close()
-        else:
-            print('connection failed')
+        while True:
+            data = self.check_msg(self.s.recv(1024))  # TODO: check if player can get more than one msg
+            if data == START:
+                self.PLAYER_STATUS = PLAYER_FREE
+                self.Indicator.turn_on_LED(GREEN_PIN)
+                # self.s.send(self.int_to_bytes(START)) #Why do we need that?
+            elif data == PLAYER_CAUGHT and self.PLAYER_STATUS == PLAYER_FREE:
+                self.PLAYER_STATUS = PLAYER_CAUGHT
+                self.Indicator.turn_on_LED(RED_PIN)
+                self.Indicator.play_sound(CAUGHT_SOUND)
+                if self.game_mode == RISE_OF_THE_DEAD:
+                    time.sleep(KNOCK_OUT_TIME-5)
+                    for i in range(10):
+                        self.Indicator.turn_off_LED()
+                        time.sleep(0.5)
+                        self.Indicator.turn_on_LED(RED_PIN)
+                    self.PLAYER_STATUS = PLAYER_FREE
+                    self.s.send(self.int_to_bytes(PLAYER_FREE))
 
+        #### self.s.close()
+
+def show_its_on(): #todo
+    """
+    turn on blue led, wait 0.3 s and turn off
+     (requires to make turn off led function)
+    """
+    pass
 
 if __name__ == "__main__":
-    player = Player()
-    player.play()
+    show_its_on()
+    try:
+        player = Player()
+        player.make_connection()
+        player.play()
+    except Exception as e: pass #todo: print to logsfile + light red led
+
